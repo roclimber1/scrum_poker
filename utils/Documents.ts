0287949783ee960352a938f2f51bf647d9ae5440
roclimber <roclimber1@gmail.com>
2023-03-16 12:18:06 +0600

    + core.ignorecase false (for git)

---
+++ utils/Documents.ts
@@ -0,0 +1,82 @@
+
+
+import { collection, doc, getDoc, addDoc, getDocs, query, where, deleteDoc } from 'firebase/firestore'
+
+
+
+import type { Firestore, CollectionReference, DocumentData, QuerySnapshot, Query } from 'firebase/firestore'
+
+
+
+
+
+export class Documents {
+
+    public docsRef: CollectionReference<DocumentData>
+
+
+    constructor(
+        public firestore: Firestore,
+        private collectionName: string
+    ) {
+
+        this.docsRef = collection(this.firestore, this.collectionName)
+    }
+
+
+    public async getDocumentById(id: string): Promise<DocumentData | null> {
+
+        const docRef = doc(this.firestore, this.collectionName, id)
+        const docSnap = await getDoc(docRef)
+
+        let data: DocumentData | null = null
+
+        if (docSnap.exists()) {
+
+            data = docSnap.data()
+        }
+
+        return data
+    }
+
+
+    public async deleteDocumentById(id: string): Promise<void> {
+
+        const docRef = doc(this.firestore, this.collectionName, id)
+
+        await deleteDoc(docRef)
+    }
+
+
+    private convertSnapshotToDocumentsList(querySnapshot: QuerySnapshot<DocumentData>): Array<DocumentData> {
+
+        const documents: Array<DocumentData> = []
+
+        querySnapshot?.forEach((doc) => {
+
+            documents.push({
+                ...doc.data(),
+                id: doc.id
+            })
+        })
+
+        return documents
+    }
+
+
+    public async getDocumentsByQuery(query: Query) {
+
+        const querySnapshot: QuerySnapshot<DocumentData> = await getDocs(query)
+
+        return this.convertSnapshotToDocumentsList(querySnapshot)
+    }
+
+
+    public async getDocumentsList(): Promise<Array<DocumentData> | null> {
+
+        const querySnapshot: QuerySnapshot<DocumentData> = await getDocs(this.docsRef)
+
+        return this.convertSnapshotToDocumentsList(querySnapshot)
+    }
+
+}
